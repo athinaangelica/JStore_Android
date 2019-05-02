599576ecbee52e2f1d527debaf960c49f3ad974b
Athina Maria A <3rr0rc4t@gmail.com>
2019-05-02 13:04:39 +0700

    Modul 9 TP

---
+++ app/src/main/java/praktikum/oop/jstore_android_athina/RegisterActivity.java
@@ -0,0 +1,60 @@
+package praktikum.oop.jstore_android_athina;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        final EditText nameInput = (EditText) findViewById(R.id.name_input);
+        final EditText emailInput = (EditText) findViewById(R.id.email_input);
+        final EditText passwordInput = (EditText) findViewById(R.id.password_input);
+        final Button registerButton = (Button) findViewById(R.id.register_button);
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final String name = nameInput.getText().toString();
+                final String email = emailInput.getText().toString();
+                final String password = passwordInput.getText().toString();
+
+                Response.Listener<String> responseListener = new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            if (jsonResponse != null) {
+                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                builder.setMessage("Register Success!").create().show();
+                            }
+                        }
+                        catch (JSONException e) {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            builder.setMessage("Register Failed!").create().show();
+                        }
+                    }
+                };
+
+                RegisterRequest registerRequest = new RegisterRequest(name, email, password, responseListener);
+
+                RequestQueue queue = Volley.newRequestQueue(RegisterActivity.this);
+                queue.add(registerRequest);
+            }
+        });
+    }
+}
